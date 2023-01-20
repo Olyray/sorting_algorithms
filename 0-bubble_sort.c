54d89de799b15bb2bc6833b8af3a267cc5cef8c8
Olamide <olire96@gmail.com>
2023-01-20 23:06:27 +0100

    first tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - arrange an array using bubble sort
+ * @array: the array in question
+ * @size: the size of the array
+ */
+
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j;
+	bool sorted = true;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				sorted = false;
+			}
+		}
+		if (sorted == true)
+			break;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: double pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL)
+	return;
+
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		while (temp->prev && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
+
