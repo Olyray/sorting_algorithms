fe8f6b3174a4f6624dd67adf3d9dcb2cab33b32f
Olamide <olire96@gmail.com>
2023-01-25 20:30:26 +0100

    quick sort, finallygit add .

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,36 +1,69 @@
 #include "sort.h"
 
 /**
- *
+ * quick_sort - sort a list of integers using quicksort
+ * @array: the array to be sorted
+ * @size: the size of the array
  */
-void quick_sort(int *array, size_t size) 
+void quick_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int pivot, temp;
+	my_quick_sort(array, 0, size - 1, size);
+}
 
-	if (size < 2) 
+/**
+ * swap - swap two integers
+ * @a: the first integer to be swapped
+ * @b: the second integer to be swapped
+ */
+void swap(int *a, int *b)
+{
+	int t = *a;
+
+	*a = *b;
+	*b = t;
+}
+
+/**
+ * partition - partition an array
+ * @arr: the array to be partitioned
+ * @low: the start of the array
+ * @high: the end of the array
+ * Return: an integer
+ */
+int partition(int *arr, int low, int high)
+{
+	int pivot, i, j;
+
+	pivot = arr[high];
+	i = (low - 1);
+	for (j = low; j <= high - 1; j++)
 	{
-		return;
-	}
-
-	pivot = array[size - 1];
-	i = 0;
-
-	for (j = 0; j < size - 1; j++) 
-	{
-		if (array[j] <= pivot) 
+		if (arr[j] < pivot)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
 			i++;
+			swap(&arr[i], &arr[j]);
 		}
 	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
 
-	temp = array[i];
-	array[i] = array[size - 1];
-	array[size - 1] = temp;
+/**
+ * my_quick_sort - the real implementation of quicksort
+ * @arr: the array to be sorted
+ * @low: the beginning of the array
+ * @high: the end of the array
+ * @size: the size of the array
+ */
+void my_quick_sort(int *arr, int low, int high, int size)
+{
+	int pi;
 
-	quick_sort(array, i);
-	quick_sort(array + i + 1, size - i - 1);
+	if (low < high)
+	{
+		pi = partition(arr, low, high);
+		print_array(arr, size);
+		my_quick_sort(arr, low, pi - 1, size);
+		my_quick_sort(arr, pi + 1, high, size);
+	}
 }
--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void my_quick_sort(int *arr, int low, int high, int size);
 
 #endif
